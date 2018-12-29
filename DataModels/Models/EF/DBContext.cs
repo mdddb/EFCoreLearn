d0eecd1c1837a6a81f1578d65723b92c077bb86e
Duisenbayev Murat <Murat.Duisenbayev@ersai.kz>
2018-12-29 17:28:49 +0500

    Add project files.

---
+++ DataModels/Models/EF/DBContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModels.Models.EF
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DBContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new User.Configuration());
+        }
+    }
+}
---
+++ DataModels/Models/EF/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModels.Models.EF
+{
+    public partial class User
+    {
+        public Guid? ID { get; set; }
+        public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public int? Age { get; private set; }
+    }
+    public partial class User
+    {
+        public class Configuration : IEntityTypeConfiguration<User>
+        {
+            public void Configure(EntityTypeBuilder<User> builder)
+            {
+                builder
+                    .Property(nameof(Age))
+                    .HasComputedColumnSql($"DATEDIFF(YEAR, {nameof(BirthDate)}, GETDATE())");
+            }
+        }
+    }
+}
